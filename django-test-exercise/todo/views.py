753e2be7130d95f1dcb965e36d03c52d46304920
1F10230032 <s1F102300324@iniad.org>
2024-06-27 14:47:25 +0900

    ソート機能を追加

--- django-test-exercise/todo/views.py
+++ django-test-exercise/todo/views.py
@@ -9,7 +9,10 @@ def index(request):
         task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
 
-    tasks = Task.objects.all()
+    if request.GET.get('order') == 'due':
+        tasks = Task.objects.order_by('due_at')
+    else:
+        tasks = Task.objects.order_by('-posted_at')
 
     context = {
         'tasks': tasks
