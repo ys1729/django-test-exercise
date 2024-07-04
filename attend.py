85764dd69192781f6432d6c6248867fe1751f6f9
1F10230032 <s1F102300324@iniad.org>
2024-07-04 13:38:15 +0900

    txtファイルを追加

---
+++ attend.py
@@ -0,0 +1,11 @@
+def repeat(xs, n):
+    ans = []
+    if n < 0:
+        raise ValueError("n must be a non-negative integer")
+    if n == 0:
+        return ans
+    for i in xs:
+        for _ in range(n):
+            ans.append(i)
+    return ans
+print(repeat(['a', 'b'], -1))
